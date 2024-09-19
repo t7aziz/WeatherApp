e35e3758c649c827672ba78f53debc925e7b8176
Taha Aziz <41844893+t7aziz@users.noreply.github.com>
2024-09-18 23:50:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function displayResults (weather) {
 function dateBuilder (d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+  // comment
   let day = days[d.getDay()];
   let date = d.getDate();
   let month = months[d.getMonth()];
